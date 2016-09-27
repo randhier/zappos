d00b911b36c360785b4c8b2599133c328c8766a5
Randhier <randhier@gmail.com>
2016-09-27 00:01:29 -0400

    Initial config of maven, testng and selenium test

---
+++ src/test/java/com/randhier/app/ZapposTest.java
@@ -0,0 +1,38 @@
+package com.mycompany.app;
+
+//import junit.framework.TestCase;
+import org.testng.annotations.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+/**
+ * Created by randhier on 9/26/16.
+ */
+public class ZapposTest {
+
+    public WebDriver driver;
+
+    @Test
+    public void main() {
+        String actualTitle = driver.getTitle();
+        System.out.println(actualTitle);
+    }
+
+    @BeforeClass
+    public void setUp() {
+        // code that will be invoked when this test is instantiated
+            driver = new FirefoxDriver();
+            driver.get("http://zappos.com");
+        }
+
+    @AfterClass
+    public void tearDown() {
+        // Close the driver
+        driver.quit();
+
+    }
+    }
