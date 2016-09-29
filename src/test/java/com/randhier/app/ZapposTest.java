2310c605a0d71c9b675e754c0660b0fb28127f66
Randhier <randhier@gmail.com>
2016-09-29 02:43:07 -0400

    Full test to verify java selenium code.  Pre object model and not DRY

--- src/test/java/com/randhier/app/ZapposTest.java
+++ src/test/java/com/randhier/app/ZapposTest.java
@@ -1,34 +1,61 @@
 package com.randhier.app;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
-import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
-/**
- * Created by randhier on 9/26/16.
- */
 public class ZapposTest {
 
     public WebDriver driver;
 
     @Test
     public void main() {
-        String actualTitle = driver.getTitle();
-        System.out.println(actualTitle);
+        Assert.assertEquals(driver.getTitle(), "Online Shoes, Clothing, Free Shipping and Returns | Zappos.com");
+        WebElement menShoesLink = driver.findElement(By.cssSelector("a[class$='Shop-Men-s-Shoes']"));
+        menShoesLink.click();
+        Assert.assertEquals(driver.getTitle(), "Men's Shoes, Shoes For Men | Ships FREE at Zappos.com");
+        WebElement menAthleticLink = driver.findElement(By.cssSelector("a[class$='Popular-Men-s-Styles-Athletic']"));
+        menAthleticLink.click();
+        Assert.assertEquals(driver.getTitle(), "Shoes, Athletic, Men | Shipped Free at Zappos");
+        WebElement inov8Link = driver.findElement(By.cssSelector("a[class$='Inov-8 ']"));
+        inov8Link.click();
+        Assert.assertEquals(driver.getTitle(), "Inov-8, Sneakers & Athletic Shoes, Athletic, Men | Shipped Free at Zappos");
+        WebElement inov8ShoeLink = driver.findElement(By.cssSelector("a[class$='product product-8734874 style-3637472 r0-5-3']"));
+        inov8ShoeLink.click();
+        Assert.assertEquals(driver.getTitle(), "inov-8 F-Lite 240 Black/Dark Red/White - Zappos.com Free Shipping BOTH Ways");
+        WebElement addToCartButton = driver.findElement(By.id("addToCart"));
+        addToCartButton.click();
+        WebElement info = driver.findElement(By.cssSelector(".info div"));
+        Assert.assertEquals(info.getText().trim(), "Please select a size");
+        WebElement noSizeLink = driver.findElement(By.id("notifyMePopupLink"));
+        noSizeLink.click();
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);}
+        Assert.assertEquals(driver.getTitle(), "Zappos.com - Notify Me!");
+        WebElement emailField = driver.findElement(By.id("email"));
+        emailField.sendKeys("Test123");
+        Select select = new Select(driver.findElement(By.id("styleId")));
+        select.selectByVisibleText("Blue/Grey/White");
+        Select select1 = new Select(driver.findElement(By.id("dimensionValueIds")));
+        select1.selectByVisibleText("UK 10 (US Men's 11)");
+        WebElement cancelButton = driver.findElement(By.cssSelector(".btn.secondary"));
+        cancelButton.click();
     }
 
     @BeforeClass
     public void setUp() {
-        // code that will be invoked when this test is instantiated
             driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             driver.get("http://zappos.com");
         }
 
     @AfterClass
     public void tearDown() {
-        // Close the driver
         driver.quit();
     }
 }
