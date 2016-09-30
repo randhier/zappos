1c49a49151428ce54ba8dd68e619f0c13bd7711a
Randhier <randhier@gmail.com>
2016-09-30 10:39:42 -0400

    Descriptive locators

--- src/test/java/com/randhier/app/ZapposTest.java
+++ src/test/java/com/randhier/app/ZapposTest.java
@@ -13,17 +13,17 @@ public class ZapposTest {
 
     public WebDriver driver;
 
-    By menShoesLoc = By.cssSelector("a[class$='Shop-Men-s-Shoes']");
-    By menAthleticLoc = By.cssSelector("a[class$='Popular-Men-s-Styles-Athletic']");
-    By inov8Loc = By.cssSelector("a[class$='Inov-8 ']");
-    By inov8ShoeLoc = By.cssSelector("a[class*='product-8734874 style-3637472']");
-    By addToCartLoc = By.id("addToCart");
-    By infoLoc = By.cssSelector(".info div");
-    By noSizeLoc = By.id("notifyMePopupLink");
-    By emailLoc = By.id("email");
-    By colorLoc = By.id("styleId");
-    By sizeLoc = By.id("dimensionValueIds");
-    By cancelLoc = By.cssSelector(".btn.secondary");
+    By menShoesLink = By.cssSelector("a[class$='Shop-Men-s-Shoes']");
+    By menAthleticLink = By.cssSelector("a[class$='Popular-Men-s-Styles-Athletic']");
+    By inov8BrandLink = By.cssSelector("a[class$='Inov-8 ']");
+    By fLite240ShoeLink = By.cssSelector("a[class*='product-8734874 style-3637472']");
+    By addToCartButton = By.id("addToCart");
+    By infoPopup = By.cssSelector(".info div");
+    By noSizeLink = By.id("notifyMePopupLink");
+    By emailField = By.id("email");
+    By colorDropdown = By.id("styleId");
+    By sizeDropdown = By.id("dimensionValueIds");
+    By cancelButton = By.cssSelector(".btn.secondary");
 
 
     public WebElement getElement(By loc){
@@ -52,28 +52,28 @@ public class ZapposTest {
 
     @Test
     public void main() {
-        clickElement(menShoesLoc);
+        clickElement(menShoesLink);
         Assert.assertEquals(driver.getTitle(), "Men's Shoes, Shoes For Men | Ships FREE at Zappos.com");
-        clickElement(menAthleticLoc);
+        clickElement(menAthleticLink);
         Assert.assertEquals(driver.getTitle(), "Shoes, Athletic, Men | Shipped Free at Zappos");
-        clickElement(inov8Loc);
+        clickElement(inov8BrandLink);
         Assert.assertEquals(driver.getTitle(), "Inov-8, Sneakers & Athletic Shoes, Athletic, Men | Shipped Free at Zappos");
-        clickElement(inov8ShoeLoc);
+        clickElement(fLite240ShoeLink);
         Assert.assertEquals(driver.getTitle(), "inov-8 F-Lite 240 Black/Dark Red/White - Zappos.com Free Shipping BOTH Ways");
-        clickElement(addToCartLoc);
-        Assert.assertEquals(getElementText(infoLoc), "Please select a size");
-        clickElement(noSizeLoc);
+        clickElement(addToCartButton);
+        Assert.assertEquals(getElementText(infoPopup), "Please select a size");
+        clickElement(noSizeLink);
         for (String handle : driver.getWindowHandles()) {
             driver.switchTo().window(handle);}
         Assert.assertEquals(driver.getTitle(), "Zappos.com - Notify Me!");
-        sendTextToElement("Test123", emailLoc);
-        selectVisableTextOfElement("Blue/Grey/White", colorLoc);
-        selectVisableTextOfElement("UK 10 (US Men's 11)", sizeLoc);
+        sendTextToElement("Test123", emailField);
+        selectVisableTextOfElement("Blue/Grey/White", colorDropdown);
+        selectVisableTextOfElement("UK 10 (US Men's 11)", sizeDropdown);
         /**
          * Rather than spam zappos with requests, I decided to just hit the cancel button
          * instead of submitting.  It's their public site so I rather not.
          */
-        clickElement(cancelLoc);
+        clickElement(cancelButton);
     }
 
     @BeforeClass
